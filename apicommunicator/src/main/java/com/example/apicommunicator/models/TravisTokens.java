45a063e3dd36676731f04c4a4e91b519b668329c
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 01:14:02 +0530

    Add Models for access tokens

---
+++ apicommunicator/src/main/java/com/example/apicommunicator/models/TravisTokens.java
@@ -0,0 +1,19 @@
+package com.example.apicommunicator.models;
+
+public class TravisTokens {
+    String publicToken;
+    String privateToken;
+
+    public TravisTokens(String publicToken, String privateToken) {
+        this.publicToken = publicToken;
+        this.privateToken = privateToken;
+    }
+
+    public String getPublicToken() {
+        return publicToken;
+    }
+
+    public String getPrivateToken() {
+        return privateToken;
+    }
+}
