cc86bc856e0903f225c382ffb4d50cc2ad682d1b
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 06:41:49 +0530

    Add Submodules

--- apicommunicator/src/main/java/com/example/apicommunicator/models/submodels/ReposDefaultBranch.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/models/submodels/ReposDefaultBranch.java
@@ -1,4 +1,49 @@
 package com.example.apicommunicator.models.submodels;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ReposDefaultBranch {
+    @SerializedName("@type")
+    private String mType;
+
+    @SerializedName("@href")
+    private String mHREF;
+
+    @SerializedName("@representation")
+    private String mRepresentation;
+
+    @SerializedName("name")
+    private String mName;
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String mType) {
+        this.mType = mType;
+    }
+
+    public String getHREF() {
+        return mHREF;
+    }
+
+    public void setHREF(String mHREF) {
+        this.mHREF = mHREF;
+    }
+
+    public String getRepresentation() {
+        return mRepresentation;
+    }
+
+    public void setRepresentation(String mRepresentation) {
+        this.mRepresentation = mRepresentation;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
 }
--- apicommunicator/src/main/java/com/example/apicommunicator/models/submodels/UserPermissions.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/models/submodels/UserPermissions.java
@@ -1,4 +1,27 @@
 package com.example.apicommunicator.models.submodels;
 
+import com.google.gson.annotations.SerializedName;
+
 public class UserPermissions {
+    @SerializedName("read")
+    private boolean mRead;
+
+    @SerializedName("sync")
+    private boolean mSync;
+
+    public boolean isRead() {
+        return mRead;
+    }
+
+    public void setRead(boolean mRead) {
+        this.mRead = mRead;
+    }
+
+    public boolean isSync() {
+        return mSync;
+    }
+
+    public void setSync(boolean mSync) {
+        this.mSync = mSync;
+    }
 }
