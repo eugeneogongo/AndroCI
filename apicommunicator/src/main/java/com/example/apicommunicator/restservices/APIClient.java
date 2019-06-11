a6e9329d609854b3149c33756694e715bfd6018a
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 06:45:27 +0530

    Define rest services for API

--- apicommunicator/src/main/java/com/example/apicommunicator/restservices/APIClient.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/restservices/APIClient.java
@@ -1,4 +1,31 @@
 package com.example.apicommunicator.restservices;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class APIClient {
+    public static final String PUBLIC_BASE_URL = "https://api.travis-ci.org/";
+    public static final String PRIVATE_BASE_URL = "https://api.travis-ci.com/";
+    private static Retrofit publicRetroFit = null;
+    private static Retrofit privateRetroFit = null;
+
+    public static Retrofit getPublicClient() {
+        if (publicRetroFit == null) {
+            publicRetroFit = new Retrofit.Builder()
+                    .baseUrl(PUBLIC_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return publicRetroFit;
+    }
+
+    public static Retrofit getPrivateClient(){
+        if (privateRetroFit == null) {
+            privateRetroFit = new Retrofit.Builder()
+                    .baseUrl(PRIVATE_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return privateRetroFit;
+    }
 }
