a496f9228d50d33e9e129f88db4b84b841e4a1b3
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 06:44:15 +0530

    add Interface for API Access

--- apicommunicator/src/main/java/com/example/apicommunicator/interfaces/IUser.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/interfaces/IUser.java
@@ -1,4 +1,25 @@
 package com.example.apicommunicator.interfaces;
 
+import com.example.apicommunicator.models.UserResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+
 public interface IUser {
+    @Headers({
+            "Travis-API-Version: 3",
+            "User-Agent: MyClient/1.0.0"
+    })
+    @GET("user")
+    Call<UserResponse> getUser(@Header("Authorization") String authorization);
+
+    @Headers({
+            "Travis-API-Version: 3",
+            "User-Agent: MyClient/1.0.0"
+    })
+    @GET("user/{id}")
+    Call<UserResponse> getIndividualUser(@Path("id") int id, @Header("Authorization") String authorization);
 }
