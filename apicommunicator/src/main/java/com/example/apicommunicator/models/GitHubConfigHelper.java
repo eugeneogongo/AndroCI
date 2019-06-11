ce15b8bce9c9897ca9f2fda2a8fd74f4023960dd
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 06:43:01 +0530

    Define models for Access

--- apicommunicator/src/main/java/com/example/apicommunicator/models/GitHubConfigHelper.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/models/GitHubConfigHelper.java
@@ -1,4 +1,13 @@
 package com.example.apicommunicator.models;
 
+import com.example.apicommunicator.BuildConfig;
+
 public class GitHubConfigHelper {
+    public static String getClientID() {
+        return BuildConfig.GITHUB_CLIENT_ID;
+    }
+
+    public static String getSecret() {
+        return BuildConfig.GITHUB_SECRET;
+    }
 }
--- apicommunicator/src/main/java/com/example/apicommunicator/models/TravisTokenRequest.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/models/TravisTokenRequest.java
@@ -1,4 +1,13 @@
 package com.example.apicommunicator.models;
 
 public class TravisTokenRequest {
+    private String github_token;
+
+    public String getGitHubToken() {
+        return github_token;
+    }
+
+    public void setGitHubToken(String github_token) {
+        this.github_token = github_token;
+    }
 }
