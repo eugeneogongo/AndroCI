14209a79054aa1b9f4a35fa786c549bfab0c075b
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 06:47:48 +0530

    Change app heirarchy

--- app/src/main/java/com/siddharthks/openbuild/ui/activities/MainActivity.java
+++ app/src/main/java/com/siddharthks/openbuild/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.siddharthks.openbuild;
+package com.siddharthks.openbuild.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.siddharthks.openbuild.R;
 
 public class MainActivity extends AppCompatActivity {
 
