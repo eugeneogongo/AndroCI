c9e2e2a3cb0be50e3914ba9f0dc5f0171da17a5e
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-06-11 06:43:27 +0530

    Add Complete set of Modules

--- apicommunicator/src/main/java/com/example/apicommunicator/models/ReposResponse.java
+++ apicommunicator/src/main/java/com/example/apicommunicator/models/ReposResponse.java
@@ -1,4 +1,64 @@
 package com.example.apicommunicator.models;
 
+import com.example.apicommunicator.models.submodels.Pagination;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
 public class ReposResponse {
+    @SerializedName("@type")
+    private String mType;
+
+    @SerializedName("@href")
+    private String mHREF;
+
+    @SerializedName("@representation")
+    private String mRepresentation;
+
+    @SerializedName("@pagination")
+    private Pagination mPagination;
+
+    @SerializedName("repositories")
+    private ArrayList<RepoResponse> mRepositories;
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String mType) {
+        this.mType = mType;
+    }
+
+    public String getHREF() {
+        return mHREF;
+    }
+
+    public void setHREF(String mHREF) {
+        this.mHREF = mHREF;
+    }
+
+    public String getRepresentation() {
+        return mRepresentation;
+    }
+
+    public void setRepresentation(String mRepresentation) {
+        this.mRepresentation = mRepresentation;
+    }
+
+    public Pagination getPagination() {
+        return mPagination;
+    }
+
+    public void setPagination(Pagination mPagination) {
+        this.mPagination = mPagination;
+    }
+
+    public ArrayList<RepoResponse> getRepositories() {
+        return mRepositories;
+    }
+
+    public void setRepositories(ArrayList<RepoResponse> mRepositories) {
+        this.mRepositories = mRepositories;
+    }
 }
