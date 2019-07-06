adbaffa247e4b82d48267f29534e28b46aa8c2f6
Siddharth Sharma <ssiddharth2010@gmail.com>
2019-07-06 23:51:00 +0530

    Fresh Start

--- apicommunicator/src/main/java/com/example/apicommunicator/callbacks/IAPICallBack.java
+++
@@ -1,9 +0,0 @@
-package com.example.apicommunicator.callbacks;
-
-import android.support.annotation.NonNull;
-
-public interface IAPICallBack<T> {
-    void onSuccess(@NonNull T value);
-
-    void onError(@NonNull String errorMessage);
-}
